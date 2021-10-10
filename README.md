1916be1aed6d08e3d6fbb6082254b6c75e4d9e1a
pakpoom-suwan <pakpoomsuwan.sw@gmail.com>
2021-10-10 12:11:36 +0700

    commit

--- README.md
+++ README.md
@@ -7,4 +7,15 @@
   - Spring Web
   - Spring Data JPA
   - PostgreSQL Driver
+
+# PostgresSql
+
+  - brew install postgresql
+  - ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+  - alias pg_start="launchctl load ~/Library/LaunchAgents"
+  - alias pg_stop="launchctl unload ~/Library/LaunchAgents"
+  - pg_start
+  - pg_stop
+  - psql
+  - ref : https://gist.github.com/ibraheem4/ce5ccd3e4d7a65589ce84f2a3b7c23a3
   
---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return  args -> {
+                Student alita = new Student(
+                        1L,
+                        "Alita",
+                        "Alita@gmail.com",
+                        LocalDate.of(2000, Month.APRIL, 5),
+                        21
+                );
+
+            repository.saveAll(List.of(alita));
+        };
+    }
+}
